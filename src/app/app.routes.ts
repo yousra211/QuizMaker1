362239c1f2776650b6503551fc90d788a325967a
yousra211 <hamdaouiya2110@gmail.com>
2025-02-14 22:34:31 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CreatorComponent } from './creators/creators.component';
+
+
+export const routes: Routes = [
+
+	{path:"creators", component:CreatorComponent}, 
+	
+];
